431f0db8b0d4bdde9e52821376bc6ac6ee29940a
Didier Hémery <didierjm.hemerygmail.com>
2019-09-19 14:31:16 +0200

    add test books management func
    implement remPubsFromDeviceInternalMemory()
    implement copyPubFromAPLToDeviceInternalMemory

--- r2-testapp/src/androidTest/java/org/readium/r2/testapp/AddLocalBook.kt
+++ r2-testapp/src/androidTest/java/org/readium/r2/testapp/AddLocalBook.kt
@@ -7,6 +7,7 @@
 package org.readium.r2.testapp
 
 import android.os.Build
+import android.util.Log
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -15,6 +16,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withTagValue
 import androidx.test.filters.LargeTest
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import org.hamcrest.CoreMatchers
 import org.junit.After
 import org.junit.Before
@@ -24,6 +26,9 @@ import org.junit.runner.RunWith
 import org.readium.r2.testapp.setup.clickButtonUiAutomator
 import org.readium.r2.testapp.setup.getStr
 import org.readium.r2.testapp.setup.initTestEnv
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 @RunWith(AndroidJUnit4::class)
@@ -42,6 +47,43 @@ class AddLocalBook
     }
 
     /**
+     * Removes every single file in the device's internal storage
+     * @ /Android/data/org.readium.r2reader.test
+     */
+    fun remPubsFromDeviceInternalMemory() {
+        try {
+            getInstrumentation().context.getExternalFilesDir(null)!!.walk().forEach {
+                it.delete()
+            }
+        } catch (e: IOException) {
+            Log.e("IO ERROR", e.stackTrace.toString())
+        }
+    }
+
+    /**
+     * Writes the file in the androidTest app assets folder to the device's internal storage.
+     * @ /Android/data/org.readium.r2reader.test
+     * Logs error if it happens.
+     *
+     * pub: String - The name of the publication to add to internal memory.
+     */
+    fun copyPubFromAPKToDeviceInternalMemory(pub: String) {
+        try {
+            Log.w("PATH", getInstrumentation().context.getExternalFilesDir(null)!!.absolutePath)
+            val file = File(getInstrumentation().context.getExternalFilesDir(null), pub)
+            val ins = getInstrumentation().context.assets.open(pub)
+            val outs = FileOutputStream(file)
+            var data = ByteArray(ins.available())
+            ins.read(data)
+            outs.write(data)
+            ins.close()
+            outs.close()
+        } catch (e: IOException) {
+            Log.e("IO ERROR", e.stackTrace.toString())
+        }
+    }
+
+    /**
      * Once the device's file explorer is open, navigate into the test files' folder.
      */
     private fun goToTestFiles() {
@@ -53,7 +95,7 @@ class AddLocalBook
                 clickButtonUiAutomator(getStr(R.string.InternalStorage_SDK23))
                 clickButtonUiAutomator(getStr(R.string.FilesFolderName))
             }
-            else -> throw Exception("Unsupported Android version for this test")
+            else -> throw Exception(getStr(R.string.UnsupportedVersionTest))
         }
 
     }
@@ -66,6 +108,7 @@ class AddLocalBook
      */
     @Test
     fun importLocalCBZPublicationWorks() {
+        //copyPubsFromAPKToDeviceInternalMemory("___example.cbz")
         onView(withTagValue(CoreMatchers.`is`(getStr(R.string.tagButtonAddBook)))).perform(ViewActions.click())
         onView(withTagValue(CoreMatchers.`is`(getStr(R.string.tagButtonAddDeviceBook)))).perform(ViewActions.click())
 

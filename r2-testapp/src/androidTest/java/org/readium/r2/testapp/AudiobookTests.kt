aef53fbffe17296c1274b23452b69a3a0249258d
Didier Hémery <didierjm.hemerygmail.com>
2019-09-26 13:51:04 +0200

    add audiobook test functions. WIP

--- r2-testapp/src/androidTest/java/org/readium/r2/testapp/AudiobookTests.kt
+++ r2-testapp/src/androidTest/java/org/readium/r2/testapp/AudiobookTests.kt
@@ -50,16 +50,20 @@ class AudiobookTests {
         return activity
     }
 
+    private fun addTestAudiobook(pub: String) {
+        copyPubFromAPKToDeviceInternalMemory(pub)
+        addPubToDatabase(pub, getActivity())
+        waitFor(1000)
+
+        onView(withText(getStr(R.string.audiobookTestName))).perform(ViewActions.click())
+    }
+/*
     /**
      * Tests that the play and pause button swap as they should.
      */
     @Test
     fun testPlayPauseSwaps() {
-        val pub = getStr(R.string.audiobookTestFile)
-        copyPubFromAPKToDeviceInternalMemory(pub)
-        addPubToDatabase(pub, getActivity())
-        waitFor(1000)
-        onView(withText(getStr(R.string.audiobookTestName))).perform(ViewActions.click())
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
 
         for (x in 0..9)
             onView(withId(Is(R.id.play_pause))).perform(ViewActions.click())
@@ -67,5 +71,109 @@ class AudiobookTests {
         onView(withTagValue(Is(getStr(R.string.pauseButton)))).check(matches(isDisplayed()))
     }
 
+    /**
+     * Tests that changing orientation doesn't resume playing if stopped before.
+     */
+    @Test
+    fun testPauseSwapOrientation() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+
+        val device = UiDevice.getInstance(getInstrumentation())
+
+        onView(withId(Is(R.id.play_pause))).perform(ViewActions.click())
+
+        device.setOrientationLeft()
+        device.setOrientationNatural()
+
+        onView(withTagValue(Is(getStr(R.string.playButton)))).check(matches(isDisplayed()))
+    }
+
+
+    /**
+     * Tests that changing orientation doesn't pause the book playback.
+     */
+    @Test
+    fun testPlaySwapOrientation() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+
+        val device = UiDevice.getInstance(getInstrumentation())
+
+        device.setOrientationLeft()
+        device.setOrientationNatural()
+
+        onView(withTagValue(Is(getStr(R.string.pauseButton)))).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that a bookmark can be added
+     */
+    @Test
+    fun testBookmarkSimple() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+
+        onView(withId(Is(R.id.bookmark))).perform(ViewActions.click())
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("BOOKMARKS")).perform(ViewActions.click())
+        onView(withText("00 - Dedication")).check(matches(isDisplayed()))
+    }*/
+
+    /**
+     * Tests that the TOC works for the middle chapter
+     */
+    @Test
+    fun testTOC() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("02 - John Bunyan")).perform(ViewActions.click())
+        onView(withText("02 - John Bunyan")).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the previous chapter arrow works for the middle chapter
+     */
+    @Test
+    fun testPrevChapMiddle() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("01 - Mr. Pepys")).perform(ViewActions.click())
+        onView(withId(R.id.prev_chapter)).perform(ViewActions.click())
+        onView(withText("00 - Dedication")).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the next chapter arrow works for the middle chapter
+     */
+    @Test
+    fun testNextChapMiddle() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("01 - Mr. Pepys")).perform(ViewActions.click())
+        onView(withId(R.id.next_chapter)).perform(ViewActions.click())
+        onView(withText("02 - John Bunyan")).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the next chapter arrow works for the last chapter
+     */
+    @Test
+    fun testNextChapEnd() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("02 - John Bunyan")).perform(ViewActions.click())
+        onView(withId(R.id.next_chapter)).perform(ViewActions.click())
+        onView(withText("02 - John Bunyan")).check(matches(isDisplayed()))
+    }
+
+    /**
+     * Tests that the previous chapter arrow works for the first chapter
+     */
+    @Test
+    fun testPrevChapBeginning() {
+        addTestAudiobook(getStr(R.string.audiobookTestFile))
+        onView(withId(Is(R.id.toc))).perform(ViewActions.click())
+        onView(withText("00 - Dedication")).perform(ViewActions.click())
+        onView(withId(R.id.prev_chapter)).perform(ViewActions.click())
+        onView(withText("00 - Dedication")).check(matches(isDisplayed()))
+    }
 
 }
